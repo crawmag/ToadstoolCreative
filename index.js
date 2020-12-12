f05b803cf13e5a9cf2efbe0c0bd96ed64bdac9bb
crawmag <crawford.magen@gmail.com>
2020-12-11 21:53:51 -0600

    messed with services/overlay and clients/ carousel. kwill fix problems tomorrow

---
+++ index.js
@@ -0,0 +1,77 @@
+
+
+
+
+const mediaQuery = window.matchMedia('(max-width:1000px)');
+function mobileView(e){
+
+    
+var slideIndex = 0;
+// showSlides();
+//add the global timer variable
+var slides,dots,timer;
+
+function showSlides() {
+    var i;
+    slides = document.getElementsByClassName("mySlides");
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    //put the timeout in the timer variable
+    timer = setTimeout(showSlides, 4000); // Change image every 8 seconds
+}
+
+function plusSlides(position) {
+    //clear/stop the timer
+    clearTimeout(timer);
+    slideIndex +=position;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    else if(slideIndex<1){slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    //create a new timer
+    timer = setTimeout(showSlides, 4000);
+}
+
+function currentSlide(index) {
+    //clear/stop the timer
+    clearTimeout(timer);
+    if (index> slides.length) {index = 1}
+    else if(index<1){index = slides.length}
+    //set the slideIndex with the index of the function
+    slideIndex = index;
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[index-1].style.display = "block";  
+    dots[index-1].className += " active";
+    //create a new timer
+    timer = setTimeout(showSlides, 4000);
+}
+}
+// all of these work without the media query- i need to figure out why they are not working with a media query- probably the way i have it set up
+if (e.matches){
+    showSlides();
+    plusSlides();
+    currentSlide();
+}
+
+mediaQuery.addEventListener('change',mobileView);
+mobileView(mediaQuery);
